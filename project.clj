24c4fd737fdf15bc5ef004ccfeacce4ada3f30e1
Korny Sietsma <korny@sietsma.com>
2018-03-27 10:55:03 +0100

    updated version in leiningen - oops

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject cloc2flare "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject cloc2flare "1.0.0"
+  :description "utility to convert cloc output to d3 flare files"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
